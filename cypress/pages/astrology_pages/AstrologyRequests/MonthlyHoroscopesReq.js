fb57e3f5b6bcfd951b51e87694c508d7c9fe8b68
DavertoMunoz <34041554+DavertoMunoz@users.noreply.github.com>
2022-02-10 10:39:31 -0700

    monthly horo requests completed

--- cypress/pages/astrology_pages/AstrologyRequests/MonthlyHoroscopesReq.js
+++ cypress/pages/astrology_pages/AstrologyRequests/MonthlyHoroscopesReq.js
@@ -24,17 +24,237 @@ class MonthlyHoroscopesReq {
     })
     // Monthly Love
 
-    cy.request('/horoscope/weekly-love/aries.html').then((response) => {
+    cy.request('/horoscope/monthly-love/aries.html').then((response) => {
         expect(response.status).to.eq(200);
     })
 
     // Weekly Work
-    cy.request('/horoscope/weekly-work/aries.html').then((response) => {
+    cy.request('/horoscope/monthly-work/aries.html').then((response) => {
         expect(response.status).to.eq(200);
     })
     
     // Monthly Dating
-    cy.request('/horoscope/weekly-dating/aries.html').then((response) => {
+    cy.request('/horoscope/monthly-dating/aries.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // taurus
+    cy.request('/horoscope/monthly-overview/taurus.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/taurus.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Weekly Work
+    cy.request('/horoscope/monthly-work/taurus.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/taurus.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // gemini
+    cy.request('/horoscope/monthly-overview/gemini.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/gemini.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/gemini.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/gemini.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // cancer
+    cy.request('/horoscope/monthly-overview/cancer.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/cancer.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/cancer.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/cancer.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // leo
+    cy.request('/horoscope/monthly-overview/leo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/leo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/leo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/leo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // virgo
+    cy.request('/horoscope/monthly-overview/virgo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/virgo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/virgo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/virgo.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // libra
+    cy.request('/horoscope/monthly-overview/libra.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/libra.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/libra.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/libra.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // scorpio
+    cy.request('/horoscope/monthly-overview/scorpio.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/scorpio.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/scorpio.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/scorpio.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // sagittarius
+    cy.request('/horoscope/monthly-overview/sagittarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/sagittarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/sagittarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/sagittarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // capricorn
+    cy.request('/horoscope/monthly-overview/capricorn.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/capricorn.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/capricorn.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/capricorn.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // aquarius
+    cy.request('/horoscope/monthly-overview/aquarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/aquarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/aquarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/aquarius.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // pisces
+    cy.request('/horoscope/monthly-overview/pisces.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    // Monthly Love
+
+    cy.request('/horoscope/monthly-love/pisces.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+
+    // Monhtly Work
+    cy.request('/horoscope/monthly-work/pisces.html').then((response) => {
+        expect(response.status).to.eq(200);
+    })
+    
+    // Monthly Dating
+    cy.request('/horoscope/monthly-dating/pisces.html').then((response) => {
         expect(response.status).to.eq(200);
     })
     
@@ -43,3 +263,5 @@ class MonthlyHoroscopesReq {
 }
 
 export default MonthlyHoroscopesReq;
+
+
